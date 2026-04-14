5437a569854c2d76a96d1c6f6c5abacb373570a3
Liang Zhuang <bzhuangliang@gmail.com>
2026-04-14 11:20:34 -0700

    hardhat config only has one SEPOLIA_PRIVATE_KEY (signer), so adjust the deploy script to match the config

--- scripts/multiSigWallet.ts
+++ scripts/multiSigWallet.ts
@@ -3,8 +3,12 @@ import {network} from "hardhat";
 async function main() {
     const { ethers } = await network.connect();
 
-    const [owner1, owner2, owner3] = await ethers.getSigners();
-    const owners = [owner1.address, owner2.address, owner3.address];
+    const [deployer] = await ethers.getSigners();
+    const owners = [
+        deployer.address, 
+        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", // another account from MetaMask
+        "0x70997970C51812dc3A010C7d01b50e0d17dc79C8" // another account from MetaMask
+    ];
     const requiredConfirmations = 2n;
 
     const factory = await ethers.getContractFactory("MultiSigWallet");
